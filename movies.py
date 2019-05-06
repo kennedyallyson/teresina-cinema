cfad2adfbcc31a80aff1ed6d272d8a278254a5a8
Kennedy Allyson <kennedy01101@gmail.com>
2019-05-06 15:33:28 -0300

    Add Requests lib

--- movies.py
+++ movies.py
@@ -1,31 +1,39 @@
 import re
-import urllib.request
+
 from bs4 import BeautifulSoup as Soup
 from bottle import route, run, template
+import requests
 
 
-url = 'http://www.cinemasteresina.com.br/'
-new_url = []
-full_url = []
+def search_movies():
+    full_url = []
+    new_url = []
 
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    page = Soup(html, 'html.parser')
-    movies = page.find_all("img", src = re.compile("/uploads/movie/poster/"))
+    with requests.get('http://www.cinemasteresina.com.br/') as response:
+        html = response.text
+        page = Soup(html, 'html.parser')
+        movies = page.find_all("img", src = re.compile("/uploads/movie/poster/"))
 
-    for i in range(len(movies)):
-        full_url = "http://www.cinemasteresina.com.br{}".format(movies[i]["src"])
-        full_url = re.sub(r"midi_", "", full_url)
+        for i in range(len(movies)):
+            full_url = "http://www.cinemasteresina.com.br{}".format(movies[i]["src"])
+            full_url = re.sub(r"midi_", "", full_url)
 
-        if full_url in new_url:
-            continue
-        else:
-            new_url.append(full_url)
+            if full_url in new_url:
+                continue
+            else:
+                new_url.append(full_url)
+    return new_url
 
 
 @route('/')
 def index():
-    return template("index.html", src = new_url)
+    new_url = search_movies()
+    return template("views/index.html", src = new_url)
 
 
-run(host='localhost', port=8000)
+def main():
+    run(host='localhost', port=8000)
+
+
+if __name__=='__main__':
+    main()
